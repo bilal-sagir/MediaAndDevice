321df931a626148a176965b5cca2d2315e29918d
bilal-sagir <sagir.bilal@icloud.com>
2021-11-22 02:30:09 +0300

    select picture from gallery

--- MediaAndDevice/ViewController.swift
+++ MediaAndDevice/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imgView: UIImageView!
+    var imgPicker: UIImagePickerController!
+    
+    @IBAction func getImg(_ sender: Any)
+    {
+        imgPicker = UIImagePickerController()
+        imgPicker.delegate = self
+        
+        let butAlert = UIAlertController(title: "Pick An Image", message: "Gallery or Camera", preferredStyle: UIAlertController.Style.alert)
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) //check camera available
+        {
+            let butCamera = UIAlertAction(title: "Camera",
+                                          style: UIAlertAction.Style.default)
+            { (act) in
+                self.imgPicker.sourceType = .camera
+                self.present(self.imgPicker, animated: true, completion: nil)
+            }
+            butAlert.addAction(butCamera)
+        }
+        
+        
+        let butGalery = UIAlertAction(title: "Galery", style: UIAlertAction.Style.default)
+        { (act) in
+            self.imgPicker.sourceType = .photoLibrary
+            self.present(self.imgPicker, animated: true, completion: nil)
+            
+        }
+        
+        
+        butAlert.addAction(butGalery)
+        self.present(butAlert, animated: true, completion: nil)
+
     }
+    
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate
+{
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        
+        if let img = info[.originalImage] as? UIImage
+        {
+            imgView.image = img
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
+    {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
