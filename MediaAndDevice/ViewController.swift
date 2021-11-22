f889b2b46134883e55d3235aae574432aa1b8173
bilal-sagir <sagir.bilal@icloud.com>
2021-11-22 15:44:27 +0300

    Accelerometer data

--- MediaAndDevice/ViewController.swift
+++ MediaAndDevice/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  MediaAndDevice
-//
-//  Created by Bilal on 22.11.2021.
-//
-
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController
+{
+    @IBOutlet weak var box: UIView!
+    let motionManager = CMMotionManager()
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        if !motionManager.isAccelerometerAvailable || //check available
+            !motionManager.isGyroAvailable {return}
+        
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!)
+        { data, Error in
+            guard let mData = data else {return}
+            
+            var cen = self.box.center
+            
+            cen.x += mData.acceleration.x
+            cen.y += mData.acceleration.y
+            
+            self.box.center = cen
+            
+        }
+        
     }
-
-
 }
 
