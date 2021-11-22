7424f88de114dbbe1e145fc25a395753816f3733
bilal-sagir <sagir.bilal@icloud.com>
2021-11-22 04:55:41 +0300

    play video in UI

--- MediaAndDevice/ViewController.swift
+++ MediaAndDevice/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  MediaAndDevice
-//
-//  Created by Bilal on 22.11.2021.
-//
-
 import UIKit
+import AVFoundation
+import AVKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController
+{
+    func loadURL(_ url: URL){
+        let player = AVPlayer(url: url)
+        let ctrl = AVPlayerViewController()
+        ctrl.player = player
+        self.present(ctrl, animated: true, completion: nil)
+        player.play()
     }
 
-
+    @IBAction func online(_ sender: Any)
+    {
+        guard let url = URL(string: "http://techslides.com/demos/sample-videos/small.mp4") else {return}
+    
+        loadURL(url)
+    }
+    
+    @IBAction func library(_ sender: Any)
+    {
+        let ctrl = UIImagePickerController()
+        ctrl.sourceType = .photoLibrary
+        ctrl.mediaTypes = ["public.movie"]
+        ctrl.delegate = self
+        
+        self.present(ctrl, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        self.dismiss(animated: true, completion: nil)
+        let url = info[.mediaURL]
+        
+        loadURL(url as! URL)
+        
+    }
+}
