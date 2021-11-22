57e9657c4c066674c25da414628abea28a94fb19
bilal-sagir <sagir.bilal@icloud.com>
2021-11-22 04:12:23 +0300

    examine recording functions

--- MediaAndDevice/ViewController.swift
+++ MediaAndDevice/ViewController.swift
@@ -1,19 +1,58 @@
-//
-//  ViewController.swift
-//  MediaAndDevice
-//
-//  Created by Bilal on 22.11.2021.
-//
-
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+{
+    var recorder: AVAudioRecorder!
+    var player: AVAudioPlayer!
+    var file: URL!
+    
+    @IBAction func recordAct(_ sender: Any)
+    {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let docsDir = urls[0] as URL
+        
+        file = docsDir.appendingPathComponent("myRecording.m4a")
+        
+        
+        let settings =
+        [
+            AVSampleRateKey: 1200,
+            AVFormatIDKey: Int (kAudioFormatMPEG4AAC),
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        
+        
+        do
+        {
+            recorder = try AVAudioRecorder(url: file, settings: settings)
+            
+            recorder.record()
+            
+        }catch{}
     }
-
-
+    
+    @IBAction func stopAct(_ sender: Any)
+    {
+        if let mRecorder = recorder
+        {
+            mRecorder.stop()
+            recorder = nil
+        }
+    }
+    
+    @IBAction func playBackAct(_ sender: Any)
+    {
+        do
+        {
+           player = try AVAudioPlayer(contentsOf: file)
+            player.prepareToPlay()
+            player.play()
+        }catch{}
+    }
 }
 
